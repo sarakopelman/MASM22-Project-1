66575060b6743c9a6969a5e4370e1976cda39825
Sara Desktop <sara@kopelman.se>
2025-04-06 17:31:55 +0200

    Estimated models

--- Code/part2.R
+++ Code/part2.R
@@ -21,4 +21,36 @@ ggplot(carotene, aes(x = smokstat, y = betaplasma, color = smokstat)) +
   geom_boxplot() +
   labs(x = "Smoking status", y="Plasma β-carotene (ng/ml)",
        color = "Smoking Status")
-  
+
+ggplot(carotene, aes(x = smokstat, y = log(betaplasma), color = smokstat)) +
+  geom_boxplot() +
+  labs(x = "Smoking status", y="Plasma β-carotene (ng/ml)",
+       color = "Smoking Status")
+#Fit models
+#Model 2b
+model2b <- lm(log(betaplasma)~smokstat,data=carotene)
+summary(model2b)
+#Alternative model
+carotene_relev <- mutate(carotene,smokstat = relevel(smokstat,"Current Smoker"))
+model2b_alt <- lm(log(betaplasma)~smokstat,data=carotene_relev)
+summary(model2b_alt)
+
+#Expected values and confidence intervals for both models
+carotene_x0 <- data.frame(smokstat = c("Never", "Former", "Current Smoker"))
+cbind(carotene_x0, fit=predict(model2b,newdata=carotene_x0),
+      conf = predict(model2b, newdata = carotene_x0, interval = "confidence")) |>
+  mutate(df = NULL,
+         residual.scale=NULL,
+         conf.fit=NULL) ->
+  carotene_x0_pred
+carotene_x0_pred
+cbind(smokstat=carotene_x0_pred[, 1],exp(carotene_x0_pred[, 2:ncol(carotene_x0_pred)]))
+
+cbind(carotene_x0, fit=predict(model2b_alt,newdata=carotene_x0),
+      conf = predict(model2b_alt, newdata = carotene_x0, interval = "confidence")) |>
+  mutate(df = NULL,
+         residual.scale=NULL,
+         conf.fit=NULL) ->
+  carotene_x0_pred_alt
+carotene_x0_pred_alt
+cbind(smokstat=carotene_x0_pred_alt[, 1],exp(carotene_x0_pred_alt[, 2:ncol(carotene_x0_pred_alt)]))
